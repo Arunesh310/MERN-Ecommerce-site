e591c99e38120151bf8e2825e14b3bcab78da3d6
Nirbhay-sys <119494283+Nirbhay-sys@users.noreply.github.com>
2024-04-06 21:20:44 +0530

    made product screen for product details

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,11 +12,14 @@ import "./assets/styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import products from "./products"; 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
 );
@@ -24,7 +27,7 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} products={products} />
   </React.StrictMode>
 );
 
